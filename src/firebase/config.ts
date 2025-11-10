6b8ad4e046e40b0d1dd615d3c5e4df9c88503963
Aditya Jamwal <adityajamwal521@gmail.com>
2025-11-10 23:01:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4002648240-5a798",
+  "appId": "1:422188552613:web:43c649aac69f24e035d84c",
+  "apiKey": "AIzaSyBDUQbubQqIGlGYMo60MPS8mzE_Ywjap4w",
+  "authDomain": "studio-4002648240-5a798.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "422188552613"
+};
